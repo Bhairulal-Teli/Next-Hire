7f0d9fdbdf768a20a2f64cc1e2c211843d3d990c
Bhairulal Teli <bhairulalteli07@gmail.com>
2026-01-29 11:31:06 +0530

    minor typo error

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -7,18 +7,18 @@ export const protectRoute = [
     async (req, res, next) => {
         try {
             const clerkId = req.auth().userId;
-            if(!clerkId) return res.status(401).json({msg: "Unauthorized - invalid token"})
+            if(!clerkId) return res.status(401).json({message: "Unauthorized - invalid token"});
 
             // find user in mongoDB
             const user = await User.findOne({clerkId});
-            if(!user) return res.status(404).json({msg: "User not found"});
+            if(!user) return res.status(404).json({message: "User not found"});
 
             // attach user to req
             req.user = user;
             next();
         } catch (error) {
             console.error("Error in protectRoute middleware", error);
-            res.status(500).json({msg: "Internal server error"});
+            res.status(500).json({message: "Internal server error"});
         }
     }
 ]
