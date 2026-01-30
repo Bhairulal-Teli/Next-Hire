298f47dc713970901becc33bb4f0cb3a764a7d3b
Bhairulal Teli <bhairulalteli07@gmail.com>
2026-01-30 17:15:13 +0530

    frotend tools setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/clerk-react";
-import "./App.css";
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import {Toaster} from "react-hot-toast";
+
+import HomePage from "./pages/HomePage";
+import ProblemsPage from "./pages/ProblemsPage";
 
 function App() {
+
+  const {isSignedIn} = useUser()
   return (
     <>
-      <h1>Welcome to the Application</h1>
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
-      <UserButton />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+    </Routes>
+    <Toaster toastOptions={{duration: 3000}} />
     </>
   );
 }
